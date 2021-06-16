f7e29dbe416015915c90a70d8a75de5d291b1d6d
TukhvatullinTR <TukhvatullinTR>
2021-06-17 00:45:42 +0500

    Start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header>Header</header>
+      <main>Content</main>
+      <footer>Footer</footer>
+    </div>
+  );
+};
+
+export default App;
