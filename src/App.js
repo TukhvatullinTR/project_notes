8ec091040682cb0967ca27e2bbc6fa6db3b61016
TukhvatullinTR <TukhvatullinTR>
2021-07-12 11:54:03 +0500

    App UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
+import Header from "./components/UI/Header";
+import Container from "./components/UI/Container";
+import SideBar from "./components/UI/SideBar";
+import ListView from "./components/UI/ListView";
+import Footer from "./components/UI/Footer";
 
 const App = () => {
+  let Items = [];
+  Array.from(Array(100).keys()).map((i) =>
+    Items.push({ id: i, title: `ListView Item ${i}` })
+  );
+
   return (
     <div className="App">
-      <header>Header</header>
-      <main>Content</main>
-      <footer>Footer</footer>
+      <Header>Header</Header>
+      <Container>
+        <SideBar>
+          <ListView title="Projects" items={Items} />
+        </SideBar>
+        <section>Content</section>
+      </Container>
+      <Footer>Footer</Footer>
     </div>
   );
 };
